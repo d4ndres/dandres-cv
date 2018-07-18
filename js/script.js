be72b1392f98b498c940a215679cfba83ce5c6eb
dandres <d4ndres@gmail.com>
2018-07-18 02:08:41 -0500

    new style 50%

--- js/script.js
+++ js/script.js
@@ -1,75 +1,105 @@
-// ---------------------modal---------------------------
-const $overlay = document.getElementById('overlay')
-const $modal = document.getElementById('modal')
-const $btn = document.getElementById('btn')
 
-$overlay.addEventListener( 'click' , close )
-$modal.addEventListener( 'animationend', close )
-
-function close(){
-  $overlay.classList.remove('active')
-}
-
-// ------------------------------------------------------
-const $downDescriptionHeph = document.getElementById('down-description-heph')
-const $downDescriptionShowPhotos = document.getElementById('down-description-show-photos')
-const $descriptionHeph = document.getElementById('images-description-down-heph')
-const $descriptionShowPhotos = document.getElementById('images-description-down-show-photos')
+// Prototipe buttons
+class btnChanger{
 
 
-$downDescriptionShowPhotos.addEventListener( 'click' , () => $descriptionShowPhotos.classList.toggle('active') )
-$downDescriptionHeph.addEventListener( 'click' , () => $descriptionHeph.classList.toggle('active') )
-
-// ------------------------------------------------------
-// codigo mejorado en un objeto
-class InteractiveBtn{
-  constructor( btn , imgPc, subTitle, imgPhone){
+  constructor(btn , imgDesktop, imgCellPhone){
     this.btn = btn
-    this.imgPc = imgPc  //state = true
-    this.imgPhone = imgPhone || undefined//state = false
-    this.subTitle = subTitle
+    this.imgDesktop = imgDesktop // state = true
+    this.imgCellPhone = imgCellPhone || undefined //state = flase
     this.state = true
-    this.subTitle.innerHTML = 'Desktop'
-    this.btn.innerHTML = 'Celular'
-    try {
-      this.imgPhone.style.display = 'none'
-      this.btn.addEventListener('mouseup', () => this.onClcik())
-    } catch(error){
-      this.btn.style.display = 'none'
-    };
+    this.imgCellPhone.style.display = 'none'
+    this.btn.addEventListener('mouseup', () => this.onClcik())
   }
   onClcik(){
-    this.state = !this.state
-    this.togglesValue()
-  }
 
-  togglesValue(){
-    if (this.state && ( this.imgPc != this.imgPhone )) {
-        this.imgPc.style.display = 'initial'
-        this.imgPhone.style.display = 'none'
-        this.btn.innerHTML = 'Celular'
-        this.subTitle.innerHTML = 'Desktop'
+    this.state = !this.state
+    if (this.state) {
+      this.imgDesktop.style.display = 'initial'
+      this.imgCellPhone.style.display = 'none'      
+      this.btn.style.background = 'radial-gradient(circle, red, #000)'
     } else {
-        this.imgPc.style.display = 'none'
-        this.imgPhone.style.display = 'initial'
-        this.subTitle.innerHTML = 'Celular'
-        this.btn.innerHTML = 'Desktop'
+      this.imgDesktop.style.display = 'none'
+      this.imgCellPhone.style.display = 'initial'
+      this.btn.style.background = '#ff0000'
     }
   }
 }
-// heph
-const $btnHeph = document.getElementById('btn-heph')
+
+class btnSlideDescription{
+  constructor(btn, slide){
+    this.btn = btn
+    this.slide = slide
+    this.state = false 
+    this.btn.addEventListener('mouseup', () => this.onClcik())
+  }
+  onClcik(){
+    this.state = !this.state
+    if (this.state) {
+      this.slide.style.width = '50vw'
+      this.slide.style.fontSize = '16px'
+      this.btn.style.background = '#0078FF'
+    } else {
+      this.slide.style.width =  '0'
+      this.slide.style.fontSize = '0' 
+      this.btn.style.background = 'radial-gradient(circle, #0078FF, #000)'
+    }
+
+
+
+  }
+}
+
+
+
+//heph
+const $btnHeph = document.getElementById('change-btn')
 const $pcImaHeph = document.getElementById('pc-image-heph')
 const $phoneImaHeph = document.getElementById('phone-image-heph')
-const $imgTitleHeph = document.getElementById('images-title-heph')
 
-const btnHeph = new InteractiveBtn( $btnHeph, $pcImaHeph, $imgTitleHeph, $phoneImaHeph )
+const $btnSlideHeph = document.getElementById('btn-slide-heph')
+const $descriptionSlideHeph = document.getElementById('description-slide-heph')
+
+
+const hephChanger = new btnChanger( $btnHeph, $pcImaHeph, $phoneImaHeph )
+const hephSlide = new btnSlideDescription ( $btnSlideHeph, $descriptionSlideHeph)
+
+
+// class InteractiveBtn{
+//   constructor( btn , imgPc, imgPhone){
+//     this.btn = btn
+//     this.imgPc = imgPc  //state = true
+//     this.imgPhone = imgPhone || undefined//state = false
+//     this.state = true
+//     try {
+//       this.imgPhone.style.display = 'none'
+//       this.btn.addEventListener('mouseup', () => this.onClcik())
+//     } catch(error){
+//       this.btn.style.display = 'none'
+//     };
+//   }
+//   onClcik(){
+//     this.state = !this.state
+
+//     if (this.state && ( this.imgPc != this.imgPhone )) {
+//       this.imgPc.style.display = 'initial'
+//       this.imgPhone.style.display = 'none'
+
+//     } else {
+//       this.imgPc.style.display = 'none'
+//       this.imgPhone.style.display = 'initial'
+
+//     }
+//   }
+// }
+// heph
+
 // showPhotos
 
-const $btnShowPhotos = document.getElementById('btn-show-photos')
-const $pcImaShowPhotos= document.getElementById('pc-image-show-photos')
-const $imgTitleShowPhotos= document.getElementById('images-title-show-photos')
+// const $btnShowPhotos = document.getElementById('btn-show-photos')
+// const $pcImaShowPhotos= document.getElementById('pc-image-show-photos')
+// const $imgTitleShowPhotos= document.getElementById('images-title-show-photos')
 
-const btnShowPhotos = new InteractiveBtn( $btnShowPhotos, $pcImaShowPhotos, $imgTitleShowPhotos)
+// const btnShowPhotos = new InteractiveBtn( $btnShowPhotos, $pcImaShowPhotos, $imgTitleShowPhotos)
 
 // ------------------------------------------------------
